4a54cb27c4bba59fc74b71f8222f753ba7505ae8
andy <andy@webcalculator.com>
2021-03-06 20:27:29 +0000

    Benchmark makemove and perft

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,30 +1,31 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use flouder::bitboard::*;
 use flouder::board::*;
-use flouder::board::movegen::perft;
+use flouder::board::movegen::*;
+use flouder::board::makemove::*;
 use flouder::board::catalog::*;
 
 fn bitwise_handcrafted(c: &mut Criterion) {
+    let n1 = 1u64 << 3;
+    let n2 = 1u64 << 4;
+    let n3 = 1u64 << 5;
     c.bench_function("bitwise_handcrafted", |b| {
         b.iter(|| {
-            let n1 = black_box(16);
-            let n2 = black_box(32);
-            let n3 = black_box(64);
-            let a = (1 << n1) | (1 << n2);
-            let b = a & (1 << n3) ;
+            let a = black_box(n1) | black_box(n2);
+            let b = a & black_box(n3) ;
             black_box(b);
         });
     });
 }
 
 fn bitwise_bitflags(c: &mut Criterion) {
+    let n1 = Bitboard::D1;
+    let n2 = Bitboard::E1;
+    let n3 = Bitboard::F1;
     c.bench_function("bibitwise_bitflags", |b| {
         b.iter(|| {
-            let n1 = black_box(Bitboard::D1.bits());
-            let n2 = black_box(Bitboard::E1.bits());
-            let n3 = black_box(Bitboard::F1.bits());
-            let a = Bitboard::from_bits_truncate(n1) | Bitboard::from_bits_truncate(n2);
-            let b = a & Bitboard::from_bits_truncate(n3);
+            let a = black_box(n1) | black_box(n2);
+            let b = a & black_box(n3);
             black_box(b);
         });
     });
@@ -49,16 +50,27 @@ fn piece_to_char(c: &mut Criterion) {
 }
 
 fn benchmark_perft(c: &mut Criterion) {
+    let board = Catalog::starting_position();
     c.bench_function("perft", |b| {
         b.iter(|| {
-            let results = vec!{ 20, 400, 8902, 197281, 4865609 };
-            let board = Catalog::starting_position();
             black_box(perft(&board, black_box(5)));
+        });
+    });
+}
+
+fn make_move(c: &mut Criterion) {
+    let board = Catalog::starting_position();
+    let mv1 = board.validate_uci_move("e2e4").unwrap();
+    let mv2 = board.validate_uci_move("b1c3").unwrap();
+    c.bench_function("makemove", |b| {
+        b.iter(|| {
+            black_box(board.make_move( black_box(&mv1)));
+            black_box(board.make_move( black_box(&mv2)));
         });
     });
 }
 
 
 
-criterion_group!(benches, bitwise_handcrafted, bitwise_bitflags, piece_to_upper_char, piece_to_char,benchmark_perft);
+criterion_group!(benches, bitwise_handcrafted, bitwise_bitflags, piece_to_upper_char, piece_to_char,benchmark_perft,make_move);
 criterion_main!(benches);
