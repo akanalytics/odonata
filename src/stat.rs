d6654750c06eb351823da0afc635625fca7bb811
andy <andy@webcalculator.com>
2021-04-12 19:36:36 +0100

    Formatting plystat

--- src/stat.rs
+++ src/stat.rs
@@ -15,6 +15,19 @@ struct PlyStat {
 }
 
 
+struct ArrayStat<'a> (&'a [&'a Stat]);
+
+impl fmt::Display for ArrayStat<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        writeln!(f, "{:<20} {:>11}", "name", "value")?;
+        writeln!(f, "{:<20} {:>11}", "--------------------", "-----------")?;
+        for s in self.0.iter() {
+            writeln!(f, "{:<20} {:>11}", s.name(), s.value())?;
+        }
+        Ok(())
+    }
+}
+
 impl Stat {
     pub fn new(name: &str) -> Stat {
         Stat { name: name.to_string(), ..Stat::default() }
@@ -35,46 +48,21 @@ impl Stat {
     pub fn name(&self) -> &str {
         &self.name
     }
+}
 
-    pub fn display(f: &mut fmt::Formatter, stats: &[&PlyStat]) -> fmt::Result {
-        let max_len = stats.iter().map(|ps| ps.len() ).max().unwrap();
-        Self::fmt_header(f, stats)?;
-        Self::fmt_underline(f, stats)?;
-        for p in 0..max_len {
-            Self::fmt_data(f, stats, p as u32)?;
-        }
-        Ok(())
+struct ArrayPlyStat<'a> (&'a [&'a PlyStat]);
 
-
-    fn fmt_header(f: &mut fmt::Formatter, stats: &[&Stat]) -> fmt::Result {
-        for s in stats.iter() {
-            write!(f, "{value}", value = s.name(),)?;
-        }
-        Ok(())
-    }
-
-    fn fmt_underline(f: &mut fmt::Formatter, stats: &[&Stat]) -> fmt::Result {
-        for s in stats.iter() {
-            write!(f, "{value}", value = "----------",)?;
-        }
-        Ok(())
-    }
-
-    fn fmt_data(&self, f: &mut fmt::Formatter, stats: &[&Stat]) -> fmt::Result {
-        for s in stats.iter() {
-            write!(f, "{value}", value = s.value(),)?;
-        }
-        Ok(())
+impl fmt::Display for ArrayPlyStat<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        PlyStat::display(f, self.0)
     }
 }
 
 
-
 impl PlyStat {
     pub fn new(name: &str) -> PlyStat {
-
         let mut vec = Vec::with_capacity(MAX_PLY);
-        [0..MAX_PLY].iter().for_each(|_| vec.push(AtomicI64::new(0)));
+        (0..MAX_PLY).into_iter().for_each(|_| vec.push(AtomicI64::new(0)));
         Self { 
             name: name.to_string(), 
             values: vec.into_boxed_slice(),
@@ -89,7 +77,7 @@ impl PlyStat {
     }
 
 
-    pub fn display(f: &mut fmt::Formatter, stats: &[&PlyStat]) -> fmt::Result {
+    fn display(f: &mut fmt::Formatter, stats: &[&PlyStat]) -> fmt::Result {
         let max_len = stats.iter().map(|ps| ps.len() ).max().unwrap();
         Self::fmt_header(f, stats)?;
         Self::fmt_underline(f, stats)?;
@@ -118,22 +106,25 @@ impl PlyStat {
 
     fn fmt_header(f: &mut fmt::Formatter, stats: &[&PlyStat]) -> fmt::Result {
         for s in stats.iter() {
-            write!(f, "{value:>11}", value = s.name(),)?;
+            write!(f, "{:>3} {:>14}", "ply", s.name(),)?;
         }
+        writeln!(f)?;
         Ok(())
     }
 
     fn fmt_underline(f: &mut fmt::Formatter, stats: &[&PlyStat]) -> fmt::Result {
-        for s in stats.iter() {
-            write!(f, "{value:>11}", value = "----------",)?;
+        for _s in stats.iter() {
+            write!(f, "{:>3} {:>14}", "---", "-------------",)?;
         }
+        writeln!(f)?;
         Ok(())
     }
 
     fn fmt_data(f: &mut fmt::Formatter, stats: &[&PlyStat], ply: u32) -> fmt::Result {
         for s in stats.iter() {
-            write!(f, "{value:>11}", value = s.value(ply),)?;
+            write!(f, "{:>3} {:>14}", ply, s.value(ply),)?;
         }
+        writeln!(f)?;
         Ok(())
     }
 }
@@ -146,13 +137,18 @@ mod tests {
 
     #[test]
     fn test_stat() {
+        let s1 = Stat::new("count");
+        let s2 = Stat::new("another");
+        let s3 = Stat::new("yet another");
+        s1.add(42);
+        s2.add(3);
+        s3.add(1415);
+        println!("{}", ArrayStat(&[&s1, &s2, &s3]));
+
         let ps = PlyStat::new("count-per-ply");
-        let s = Stat::new("count");
         ps.add(4, 8);
         ps.add(5, 10);
         ps.add(2, 4);
-        s.add(42);
-        println!("{}", s.display());
-        println!("{}", display(&[&ps]));
+        println!("{}", ArrayPlyStat(&[&ps]));
     }
 }
