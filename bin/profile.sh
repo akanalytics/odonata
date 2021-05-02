a3c1e8ca1f2f18d4a06d72c750b303e3fc4ad1be
andy <andy@webcalculator.com>
2021-05-02 14:43:42 +0100

    bin/profile flag

--- bin/profile.sh
+++ bin/profile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-perf record --call-graph dwarf -f --output=./tmp/perf.data  ./target/release/profile
+perf record --call-graph dwarf --output=./tmp/perf.data  ./target/release/profile
 chown andy.andy ./tmp/perf.data
 perf report --input=./tmp/perf.data
 
