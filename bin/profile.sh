9de2971ac9c66fe32db98250941b1dfe31f9c873
andy <andy@webcalculator.com>
2021-05-24 08:07:19 +0100

    profile the perft not the profile bin

--- bin/profile.sh
+++ bin/profile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-perf record --call-graph dwarf -F 99 --output=./tmp/perf.data  ./target/release/profile --perft 6
-chown andy.andy ./tmp/perf.data
+perf record --call-graph dwarf -F 99 --output=./tmp/perf.data  ./target/release/odonata --perft 6
+# chown andy.andy ./tmp/perf.data
 perf report --input=./tmp/perf.data
 
 
