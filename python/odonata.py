e4114b556d19f847a7c6ab9c22986c80fa1468b6
Andy Watkins <andy@webcalculator.com>
2021-07-08 19:44:03 +0100

    x10000 for faster pcs

--- python/odonata.py
+++ python/odonata.py
@@ -1541,7 +1541,7 @@ def calc_calls_per_second(function: Callable) -> int:
     for _ in range(N):
         function()
     elapsed = perf_counter() - start
-    return (N * 1000) // int(elapsed * 1000 + 1)
+    return (N * 10000) // int(elapsed * 10000)
 
 
 def benchmark_1():
