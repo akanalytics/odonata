c2947e1217ce40b86e648f5bee9817d0972e74da
Andy Watkins <andy@webcalculator.com>
2021-07-15 20:21:46 +0100

    debug_assert not assert

--- src/search/iterative_deepening.rs
+++ src/search/iterative_deepening.rs
@@ -147,7 +147,7 @@ impl Algo {
             // pv.truncate(depth as usize);
             sp.pv = Some(pv);
             if !self.board.is_legal_variation(&res.pv) {             
-                assert!(false, "PV  {} is invalid on board {}\n{:?}\n{}", res.pv, self.board, res.pv, self);
+                debug_assert!(false, "PV  {} is invalid on board {}\n{:?}\n{}", res.pv, self.board, res.pv, self);
                 res.pv.truncate(1);
                 let pv = res.pv.clone();
                 sp.pv = Some(pv);
