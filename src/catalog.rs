7a5614a00d525d06a32fc4dc45d94d8e5b5a203a
Andy Watkins <andy@webcalculator.com>
2021-12-13 22:09:42 +0000

    Test  cases for pawn structure

--- src/catalog.rs
+++ src/catalog.rs
@@ -247,6 +247,8 @@ r1r5/2N1n1kb/2nB1b1p/3p2p1/6q1/P2pQ3/3N1PPP/1RR3K1 b - - 11 37 acd 6;
         k7/p5p1/8/7P/3P2p1/3p4/P2PP3/K7 w - - 0 1 'PAWN.03'; c0 'isolated=2;passed=1;doubled=1'; c1 'isolated=4;passed=1;doubled=1';
         2k5/ppp2ppp/8/4P3/P7/3p4/PP5P/K7 b - - 0 1 id 'PAWN.04'; c0 'isolated=2;passed=0;doubled=1'; c1 'isolated=0;passed=1;doubled=0';
         2k5/ppp2p1p/5p2/5p1P/3P3P/3P3P/P2P3P/K7 w - - 0 1 id 'PAWN.05'; c0 'isolated=8;passed=0;doubled=5'; c1 'isolated=4;passed=3;doubled=2';
+        k7/p2p3p/3p3p/3p3p/5P1p/5P2/PPP2P1P/2K5 b - - 0 1 id 'PAWN.06'; c0 'isolated=4;passed=3;doubled=2'; c1 'isolated=8;passed=0;doubled=5';
+        8/5k2/8/6KP/2n1P3/5P2/8/8 b - - 0 85 id 'PAWN.07'; c0 'isolated=1;passed=3;doubled=0'; c1 'isolated=0;passed=0;doubled=0';       
 "#;
         let positions = Position::parse_many_epd(str.lines()).unwrap();
         positions
